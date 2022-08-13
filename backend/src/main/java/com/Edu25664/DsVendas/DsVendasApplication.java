1a025d8b15c41d9a570a087a0829a3ce04c235a6
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-13 10:56:45 -0300

    Project created

---
+++ backend/src/main/java/com/Edu25664/DsVendas/DsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.Edu25664.DsVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Edu25664/DsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edu25664.DsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
